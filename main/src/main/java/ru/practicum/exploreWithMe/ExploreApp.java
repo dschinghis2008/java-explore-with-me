9571c28607734f628ed23ea3f07089a83971d666
Dmitry <dschinghis2008@yandex.ru>
2022-11-15 13:39:25 +0700

    feat:создать модули

---
+++ main/src/main/java/ru/practicum/exploreWithMe/ExploreApp.java
@@ -0,0 +1,13 @@
+package ru.practicum.exploreWithMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreApp.class, args);
+	}
+
+}
